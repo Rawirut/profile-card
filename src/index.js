736dbacc8fdb0ee3fe8c73155b86f5ab77c47ed6
Rawirut <rawirut.da.65@ubu.ac.th>
2023-10-12 13:50:05 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App(){
+  return(
+    <>
+      <div className='card'>
+        <Avatar />
+        <div className='data'>
+          <Intro />
+          <SkillList />
+    
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      <Skill skill='Gaming' emoji='👶🏻' color='red' />
+      <Skill skill='Music' emoji='👶🏻' color='blue' />
+      <Skill skill='Eat' emoji='👶🏻' color='orange' />
+      <Skill skill='sleep' emoji='👶🏻' color='cyan' />
+
+    </div>
+
+  );
+}
+
+function Skill(props){
+  return(
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+
+    </div>
+
+  );
+}
+
+function Intro(){
+  return(
+    <div>
+      <h1>นายรวีรุจ ดานุวงค์</h1>
+      <p>
+        นักศึกษาสาขาเทคโนโลยีสารสนเทศและการสื่อสาร คณะวิทยาศาสตร์ 
+        มหาวิทยาลัยอุบลราชธานี ชอบฟังเพลงเล่นเกมและนอน
+      </p>
+
+    </div>
+
+  );
+}
+
+function Avatar() {
+  return <img className='avatar' src='arm.jpg' alt='My Avatar'></img>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +65,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
